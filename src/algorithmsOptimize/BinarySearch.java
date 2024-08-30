88f4f977569c288c4e76a6c09b490d6f92114a11
SaveliyYarovikov <savell132435@gmail.com>
2024-08-30 15:41:13 +0500

    add bugs

--- src/algorithmsOptimize/BinarySearch.java
+++ src/algorithmsOptimize/BinarySearch.java
@@ -2,6 +2,7 @@ package algorithmsOptimize;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] binarySearchArray = new int[10000];
+        int[] binarySearchArray = new int[1488];
+        System.out.println("some bugs");
     }
 }
